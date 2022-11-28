88e10171ffa673c10551935bc26a1fe68acf2d69
Elena Malinovskaya <e.zaharova.agat34@yandex.ru>
2022-11-28 16:31:20 +0300

    add solution

--- Prodject/Program.cs
+++ Prodject/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = 3;
+string[] arrayStart = SetElements();
+string[] arrayEnd = NewArray(arrayStart,n);
+PrintArray(arrayEnd);
+
+string[] SetElements()
+{
+Console.WriteLine($"Enter the elements separated by commas");
+string[] array = Console.ReadLine().Split(",");
+return array;
+}
+
+int CountElementsNoMoreN(string[] array, int lenghtElements)
+{
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= lenghtElements)
+    {
+        count++;
+    }
+}
+return count;
+}
+
+string[] NewArray(string[] array, int lenghtElements)
+{
+string[] arrayNew = new string[CountElementsNoMoreN(array, lenghtElements)];
+for (int i = 0, j = 0;  i < array.Length; i++)
+{
+    if (array[i].Length <= lenghtElements)
+    {
+        arrayNew[j] = array[i];
+        j++;
+    }
+    
+}
+return arrayNew;
+}
+
+void PrintArray(string[] arr)
+{
+    System.Console.Write($"[{String.Join(", ", arr)}]");
+}
